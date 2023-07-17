a5004338806daba61ef2e02af5b0fcd0331be154
Jacob Neeley <jacneeley@outlook.com>
2023-07-17 14:37:07 -0500

    added css

--- simple-html-template/flask_html_templates.py
+++ simple-html-template/flask_html_templates.py
@@ -1,15 +1,15 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates', static_folder='static')
 
-@app.route("/")
-def home():
-    return "<h1>Welcome to the Server</h1>"
+# @app.route("/")
+# def home():
+#     return "<h1>Welcome to the Server</h1>"
 
 lst_users = ["Admin","Jake","Tim","Eric"]
 
-@app.route("/<name>")
-def user_page(name):
+@app.route("/")
+def index():
     return render_template("index.html",content=lst_users)
 
 if __name__ == "__main__":
