33000baca250a6db2b19d4a7880c44f3cd085878
3ak <jacneeley@outlook.com>
2023-07-18 10:11:07 -0500

    add render for "welcome" page

--- simple-html-template/flask_html_templates.py
+++ simple-html-template/flask_html_templates.py
@@ -2,15 +2,15 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__,template_folder='templates', static_folder='static')
 
-# @app.route("/")
-# def home():
-#     return "<h1>Welcome to the Server</h1>"
+@app.route("/")
+def home():
+    return render_template("welcome.html")
 
 lst_users = ["Admin","Jake","Tim","Eric"]
 
-@app.route("/")
-def index():
-    return render_template("index.html",content=lst_users)
+@app.route("/owners")
+def owners_page():
+    return render_template("index.html",owners=lst_users)
 
 if __name__ == "__main__":
     app.run(debug=True)
