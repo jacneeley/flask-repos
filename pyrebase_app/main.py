37ec08158c48ffe863109f24ef0ca51ac073997b
3ak <jacneeley@outlook.com>
2023-08-07 16:17:29 -0500

    added "blueprint_home"

--- pyrebase_app/main.py
+++ pyrebase_app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from server.get_imgs import get_db_imgs
+from server.home import blueprint_home
 
 app = Flask(__name__, static_folder="server/static/")
 app.register_blueprint(get_db_imgs, url_prefix="/server")
+app.register_blueprint(blueprint_home, url_prefix="/home")
 @app.route("/")
 def test():
     return "<h1>Test Landing Page</h1>"
