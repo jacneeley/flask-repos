4463bfe4168f7d90d3b0b1be8aeee14a83653640
3ak <jacneeley@outlook.com>
2023-08-07 16:18:01 -0500

    misc changes

--- pyrebase_app/server/get_imgs.py
+++ pyrebase_app/server/get_imgs.py
@@ -4,7 +4,7 @@ from server.config import db
 
 get_db_imgs = Blueprint("get_imgs", __name__, static_folder="static", static_url_path='/static/styles/' ,template_folder="templates")
 
-@get_db_imgs.route("/home")
+@get_db_imgs.route("/server")
 @get_db_imgs.route("/")
-def home():
-    return render_template("home.html",data =db)
+def img_database():
+    return render_template("db_images.html",data =db)
