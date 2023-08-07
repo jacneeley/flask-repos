80f8467c8995848cc8a11160447437b831f7db6f
3ak <jacneeley@outlook.com>
2023-08-07 09:48:17 -0500

    defined static path & changed blueprint variables to remove ambiguity

--- pyrebase_app/main.py
+++ pyrebase_app/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-from server.get_imgs import get_imgs
+from server.get_imgs import get_db_imgs
 
-app = Flask(__name__)
-app.register_blueprint(get_imgs, url_prefix="/server")
+app = Flask(__name__, static_folder="server/static/")
+app.register_blueprint(get_db_imgs, url_prefix="/server")
 @app.route("/")
 def test():
     return "<h1>Test Landing Page</h1>"
--- pyrebase_app/server/get_imgs.py
+++ pyrebase_app/server/get_imgs.py
@@ -2,9 +2,9 @@ from flask import Blueprint, render_template
 import server.config
 from server.config import db
 
-get_imgs = Blueprint("get_imgs", __name__, static_folder="static", template_folder="templates")
+get_db_imgs = Blueprint("get_imgs", __name__, static_folder="static", static_url_path='/static/styles/' ,template_folder="templates")
 
-@get_imgs.route("/home")
-@get_imgs.route("/")
+@get_db_imgs.route("/home")
+@get_db_imgs.route("/")
 def home():
     return render_template("home.html",data =db)
